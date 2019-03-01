d4a6fe61fa156fe72aed7c36e36b455c6966df4f
Gowtham <gowtham.m6795@gmail.com>
2019-03-01 15:13:55 +0530

    Initial Commit

---
+++ dJAX_Libs/dJAX_Libs.h
@@ -0,0 +1,19 @@
+//
+//  dJAX_Libs.h
+//  dJAX_Libs
+//
+//  Created by dJAX on 01/03/19.
+//  Copyright © 2019 dJAX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for dJAX_Libs.
+FOUNDATION_EXPORT double dJAX_LibsVersionNumber;
+
+//! Project version string for dJAX_Libs.
+FOUNDATION_EXPORT const unsigned char dJAX_LibsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dJAX_Libs/PublicHeader.h>
+
+
